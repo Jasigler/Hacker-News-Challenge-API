7c164dfd7bfc6093e6986e07d3c0f74740ccc3d2
Jasigler <jason.sigler@outlook.com>
2020-12-08 15:00:27 -0500

    Added TestBase class that was orphaned

---
+++ UnitTests/TestBase.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Logging;
+using Moq;
+using System.Net.Http;
+using WebClients.Stories;
+
+namespace UnitTests
+{
+    public class TestBase
+    {
+        protected Mock<IMemoryCache> MockCacheClient = new Mock<IMemoryCache>();
+        protected Mock<ILogger<IStoryClient>> MockLoggingClient = new Mock<ILogger<IStoryClient>>();
+        protected Mock<HttpMessageHandler> MockHttpHandler = new Mock<HttpMessageHandler>();
+        protected Mock<IStoryClient> MockStoryClient = new Mock<IStoryClient>();
+    }
+} 
